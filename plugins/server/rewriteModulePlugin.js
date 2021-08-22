8b5278a2037addf06bf7bf932daa49a2ee0d5034
williamyorkl <p.1403303507@qq.com>
2021-08-22 12:39:24 +0800

    更新koa的中间件

--- plugins/server/rewriteModulePlugin.js
+++ plugins/server/rewriteModulePlugin.js
@@ -1,17 +1,18 @@
-const { readBody } = require('./utils')
+const { readBody } = require("./utils");
 
 function rewriteImports(source) {
-    // ...
+  // ...
 }
 
-module.exports = function({app, root}) {
-    app.use(async (ctx, next) => {
-        // ctx 是中间键自带的
-        await next();
+module.exports = function ({ app, root }) {
+  // app, root是解构外面传入的context
+  app.use(async (ctx, next) => {
+    // ctx 是koa中间件callback的请求上下文对象（可以理解成express中的res和req的结合）
+    await next();
 
-        if (ctx.body && ctx.response.is('js')) {
-            const content = await readBody(ctx.body);
-            ctx.body = rewriteImports(content);
-        }
-    })
-}
+    if (ctx.body && ctx.response.is("js")) {
+      const content = await readBody(ctx.body);
+      ctx.body = rewriteImports(content);
+    }
+  });
+};
